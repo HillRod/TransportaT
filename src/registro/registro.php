16e3871c3cc07b3dbf6d698589f56fd5b63ccd5d
marijosebi <40366076+marijosebi@users.noreply.github.com>
2018-09-30 05:47:10 -0500

    Update registro.php

--- src/registro/registro.php
+++ src/registro/registro.php
@@ -1,23 +1,14 @@
 <body>
-	
-
-  <section calss="formulario">
-  
-	<form class= "formsq" id="registro" action=" ">
-       
+	<div class="forms">
+	<form class="formsq"  action=" ">
 		<h2 id="titulo">REGISTRO </h2> 
         <input type="Text" class="campo" name=""  id="nombre-usuario" name="nombre-usuario" placeholder="Nombre de usuario"> <br>
          <input type="Text" class="campo" name=""  id="password" name="password" placeholder="Contraseña"> <br>
          <input type="Text" class="campo" name=""  id="tarjeta"  name="tarjeta" placeholder="No.de tarjeta "> <br>
          <label campo id="lbl_id" class="campo" value=" hola funciono">Aqui va el id</label>
-         <br><br>  
-        
-       
+         <br>
     	<input type="submit" class="submit" id="registrar" name="registrar" value="Registrar">
-   
+
 </form>	
- </section>
-
-
-
+</div>
 </body>
